d34300ba39e8630e7a1781fd5241a223aced7fbd
William <he.william@yahoo.com>
2018-01-21 23:22:21 -0800

    removed some text

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Chart from '../containers/chart'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-          Hello!
-      </div>
+        <div>
+            <Chart/>
+        </div>
     );
   }
 }
