6b10978959d7b18771900869cf913da6698794bb
William <he.william@yahoo.com>
2018-01-22 14:25:11 -0800

    fixed spacing

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Chart from '../containers/chart'
 
 export default class App extends Component {
-  render() {
-    return (
-        <div>
-            <Chart/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Chart />
+            </div>
+        );
+    }
 }
